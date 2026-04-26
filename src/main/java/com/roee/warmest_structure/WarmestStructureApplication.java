ee815b93303846a63981c7ddbf3dd6bcc9829057
Roee Slonim <roees@nli-dev.local>
2026-04-26 22:32:39 +0300

    Initial commit - Spring Boot structure

---
+++ src/main/java/com/roee/warmest_structure/WarmestStructureApplication.java
@@ -0,0 +1,13 @@
+package com.roee.warmest_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmestStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmestStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roee/warmest_structure/WarmestStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roee.warmest_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmestStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
